b33982120d156c9b137c37cf56e90669d43b1bf1
jahidulislam123 <islam15-3879@diu.edu.bd>
2023-06-16 12:31:13 +0600

    [Done] setup express, mongoose , cors , parser

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-
+//HI
 //testing
 app.get('/', (req:Request, res:Response) => {
   res.send('Working successfully ')
